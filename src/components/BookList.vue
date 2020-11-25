2fcd86c849dd55a35f3eced5df4fd41aba12bde6
Prashanth Loganathan <4logan88@gmail.com>
2020-11-25 10:48:04 +0000

    Correct the book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,22 +2,23 @@
   <div>
     <h1>{{ title }}</h1>
     <ul>
-      <li v-for="book in books" :key="book.title">
-        {{book.title} : {{book.author}
-      </li>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "BookList",
+  name: 'BookList',
   data() {
-    books: [
-      {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-      {title: 'American Gods', author: 'Neil Gaiman'},
-      {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-    ];
+    return {
+      title : 'All Books',
+      books: [
+        {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {title: 'American Gods', author: 'Neil Gaiman'},
+        {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
+    }
   },
 };
 </script>
